825120c4769f581ec2d03d7b5102f4f619486a9a
tiana-horn <tiana.horn@gmail.com>
2018-12-14 06:04:40 -0500

    Add templates, get homepge with map working. Need to get header and map to work together

--- mapfinder/urls.py
+++ mapfinder/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from core import views 
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
